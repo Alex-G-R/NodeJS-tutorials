6dbd399d6e6c5e44810711331ee95e4c476d0e96
Alex-G-R <alex.grabski555@gmail.com>
2023-09-01 17:26:45 +0200

    Adding first app and first lesson

---
+++ Scripts/1 - globals.js
@@ -0,0 +1,16 @@
+// Globals - no window !
+
+// __dirname - path to current dir
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info bout current module (file)
+// process - info about env where the program is being executed
+
+
+// Console works too!
+console.log(__dirname);
+
+// set interval and timeout works!
+setInterval(()=>{
+    console.log("Hello World!")
+}, 1000)
